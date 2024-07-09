ebdc09cf9c761149cd49512f4ca98ad1fbf4a0cc
Shubhashis Roy <shubhashisroy360@gmail.com>
2024-07-09 19:27:56 +0530

    creat action component, userpost dynamic

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,10 +1,17 @@
 import React from "react";
 import UserHeader from "../components/UserHeader";
+import UserPost from "../components/UserPost";
 
 const UserPage = () => {
   return (
     <>
       <UserHeader />
+      <UserPost
+        likes={"12k"}
+        replies={"2k"}
+        postImg={"/post1.png"}
+        postTitle={"New post"}
+      />
     </>
   );
 };
