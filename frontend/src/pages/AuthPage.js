a419c76f4cb57ba6694336f57e0edc2254b7712d
Shubhashis Roy <shubhashisroy360@gmail.com>
2024-07-28 12:50:41 +0530

    feat: login & signup form UI, set recoil

--- frontend/src/pages/AuthPage.js
+++ frontend/src/pages/AuthPage.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useRecoilValue } from "recoil";
+import SignupCard from "../components/SignupCard";
+import authScreenAtom from "../atoms/authAtom";
+import LoginCard from "../components/LoginCard";
 
 const AuthPage = () => {
-  return <div>AuthPage</div>;
+  const authScreenState = useRecoilValue(authScreenAtom);
+
+  return <>{authScreenState === "login" ? <LoginCard /> : <SignupCard />}</>;
+  //   return (
+  //     <>
+  //       <LoginCard />
+  //     </>
+  //   );
 };
 
 export default AuthPage;
